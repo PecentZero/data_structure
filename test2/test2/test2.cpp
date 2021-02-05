46769b52d6800514faa1b365919b6247a4d4de4f
PecentZero <security428@naver.com>
2021-02-05 21:41:43 +0900

    숫자를 입력받고 모든 인접 자리수 차이가 1인지 판별

--- test2/test2/test2.cpp
+++ test2/test2/test2.cpp
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX 1000
+
+
+//입력을 받고 인접한 모든 자리의 수가 1차이가 나는 지 확인 
 int main()
 {
-	printf("test2");
+	int i = 0;
+	int length;
+	char input[MAX];
+	int check = 1; // 0이면 아님 
+	scanf("%s", input);
+	length = strlen(input);
+
+	while (i < length-1)
+	{
+
+		if (((input[i] - input[i+1]) != 1)  && ((input[i] - input[i + 1]) != -1))
+		{
+			printf("%d", i);
+			check = 0;
+			break;
+		}
+		i++;
+
+	}
+
+	if (check)
+	{
+		printf("맞음");
+	}
+	else
+		printf("아님");
+
 }
